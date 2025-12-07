96a621b92d71be6ea283b5e09492fa848ab18fef
Ian Lalusin <ianlalusin@gmail.com>
2025-12-07 14:55:49 +0000

    use this bucket gs://shareat-hub.firebasestorage.app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBnULT1PqmucOQ8Uujvw8SPYqUfkSk8nkA",
   authDomain: "shareat-hub.firebaseapp.com",
   projectId: "shareat-hub",
-  storageBucket: "shareat-hub.appspot.com",
+  storageBucket: "shareat-hub.firebasestorage.app",
   messagingSenderId: "539635723758",
   appId: "1:539635723758:web:f36aa634a225025a9cf2ff",
   measurementId: "G-SYQZC1PN6G"
