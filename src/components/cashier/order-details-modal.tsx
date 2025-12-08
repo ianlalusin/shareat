f7d6455661a1ed153d88473b639844514a9ba5ab
Ian Lalusin <ianlalusin@gmail.com>
2025-12-08 08:06:06 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/components/cashier/order-details-modal.tsx
+++ src/components/cashier/order-details-modal.tsx
@@ -14,6 +14,7 @@ import {
   DialogDescription,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
 import { Separator } from '@/components/ui/separator';
 import { AlertTriangle, BellRing, CheckCircle, Hourglass, Plus, Minus, Trash2 } from 'lucide-react';
 import { cn } from '@/lib/utils';
