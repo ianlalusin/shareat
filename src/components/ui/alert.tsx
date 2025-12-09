67affeff708d2ac4a83d50e66376e4e07ac71430
Ian Lalusin <ianlalusin@gmail.com>
2025-12-09 03:20:49 +0000

    Updated Alert component (with variants + sizes)
    Here’s a drop-in replac

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,24 +1,37 @@
 
+"use client"
+
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
     variants: {
       variant: {
         default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-        success: "border-green-500/60 bg-green-50 text-green-800 dark:bg-green-900/30 dark:text-green-100 [&>svg]:text-green-600",
-        warning: "border-yellow-500/60 bg-yellow-50 text-yellow-900 dark:bg-yellow-900/30 dark:text-yellow-50 [&>svg]:text-yellow-600",
-        info: "border-blue-500/40 bg-blue-50 text-blue-900 dark:bg-blue-900/30 dark:text-blue-50 [&>svg]:text-blue-500",
+        success:
+          "border-green-500/60 bg-green-50 text-green-900 " +
+          "dark:bg-green-900/30 dark:text-green-50 [&>svg]:text-green-600",
+        warning:
+          "border-yellow-500/60 bg-yellow-50 text-yellow-900 " +
+          "dark:bg-yellow-900/30 dark:text-yellow-50 [&>svg]:text-yellow-600",
+        info:
+          "border-blue-500/50 bg-blue-50 text-blue-900 " +
+          "dark:bg-blue-900/30 dark:text-blue-50 [&>svg]:text-blue-500",
+      },
+      size: {
+        sm: "p-3 text-xs",
+        md: "p-4 text-sm",
       },
     },
     defaultVariants: {
       variant: "default",
+      size: "md",
     },
   }
 )
@@ -26,11 +39,11 @@ const alertVariants = cva(
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, variant, ...props }, ref) => (
+>(({ className, variant, size, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
-    className={cn(alertVariants({ variant }), className)}
+    className={cn(alertVariants({ variant, size }), className)}
     {...props}
   />
 ))
