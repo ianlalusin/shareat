1be6c74b7bb55e13d81a996e0d918a63ed1459ce
Ian Lalusin <ianlalusin@gmail.com>
2025-12-09 12:15:55 +0000

    1. Make the month width tighter
    In Calendar (@/components/ui/calendar),

--- src/components/admin/date-range-picker.tsx
+++ src/components/admin/date-range-picker.tsx
@@ -118,7 +118,7 @@ export function DateRangePicker({
               )}
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="end">
+          <PopoverContent className="w-auto p-0 max-w-[700px]" align="center">
             <Calendar
               initialFocus
               mode="range"
