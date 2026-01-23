7c061a0cd0f0cbdef939a692f06cc0c4ccd441fb
Ian Lalusin <ianlalusin@gmail.com>
2026-01-23 01:40:43 +0000

    seems like no data is fetched by logs page now it is not showing any dat

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -124,7 +124,8 @@ export default function LogsPage() {
     setIsLoading(true);
 
     const logsQuery = query(
-        collection(db, "stores", activeStore.id, "activityLogs"),
+        collectionGroup(db, "activityLogs"),
+        where("storeId", "==", activeStore.id),
         where("createdAt", ">=", Timestamp.fromDate(start)),
         where("createdAt", "<=", Timestamp.fromDate(end)),
         orderBy("createdAt", "desc")
