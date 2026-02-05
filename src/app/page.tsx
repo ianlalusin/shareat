30be444943a14b36690c77e420bc765c7bec66d0
Ian Lalusin <ianlalusin@gmail.com>
2026-02-05 00:50:02 +0000

    [error] The web assets directory (./out) must contain an index.html file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-"use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { BrandLoader } from "@/components/ui/BrandLoader";
+import LoginPage from "./login/page";
 
 export default function RootPage() {
-    const router = useRouter();
-
-    useEffect(() => {
-        router.replace('/login');
-    }, [router]);
-
-    return (
-        <div className="w-full min-h-screen flex items-center justify-center bg-background">
-            <BrandLoader />
-        </div>
-    );
+  return <LoginPage />;
 }
